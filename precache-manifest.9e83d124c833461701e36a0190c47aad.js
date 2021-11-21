5ba796e669d9014df98551f08f23d738030301a1
sottomarino <simone.ambrosi@live.com>
2021-11-21 12:31:22 +0100

    Updates

---
+++ precache-manifest.9e83d124c833461701e36a0190c47aad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a23412f68db79c9f64dc0ef0baffb1",
+    "url": "/dvideo-master/index.html"
+  },
+  {
+    "revision": "6b31e71cbe2d0b073674",
+    "url": "/dvideo-master/static/css/2.80dce9aa.chunk.css"
+  },
+  {
+    "revision": "6b08fa9deaa08e44c7b6",
+    "url": "/dvideo-master/static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "6b31e71cbe2d0b073674",
+    "url": "/dvideo-master/static/js/2.23c65abf.chunk.js"
+  },
+  {
+    "revision": "30545a24812bc2557c4d652dede4cf2a",
+    "url": "/dvideo-master/static/js/2.23c65abf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b08fa9deaa08e44c7b6",
+    "url": "/dvideo-master/static/js/main.cbf2d7bd.chunk.js"
+  },
+  {
+    "revision": "f1bc8d5115d1f45288ce",
+    "url": "/dvideo-master/static/js/runtime-main.1c24a761.js"
+  }
+]);
